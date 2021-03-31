399a83fc23d935df145e1894976928f2b0bfc740
Chizim Chinuru <garrohat@yahoo.co.uk>
2021-03-31 14:50:33 +0100

    setting up pipeline

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Job Listing with Filtering Functionality</h1>
+      <p>This site is undergoing construction</p>
+    </div>
+  );
+}
+
+export default App;
